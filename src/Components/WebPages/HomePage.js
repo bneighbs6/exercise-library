b76498d034bf7347e8ca4c890b2eee087e5af05f
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-04-10 17:52:49 -0600

    Set up Routes&Files for each web page in index.js

---
+++ src/Components/WebPages/HomePage.js
@@ -0,0 +1,4 @@
+/* 
+--- START HERE MOTHAFUCKA ---
+You need to set up what your home page will look like
+*/
--- src/Layout/index.js
+++ src/Layout/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import Header from "../Components/Header";
 import Navbar from "../Components/Navbar";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "../Components/HomePage"
+import HomePage from "../Components/WebPages/HomePage";
+import AboutPage from "../Components/WebPages/AboutPage";
+import PushPage from "../Components/WebPages/PushPage";
+import PullPage from "../Components/WebPages/PullPage";
+import HipPage from "../Components/WebPages/HipPage";
+import KneePage from "../Components/WebPages/KneePage";
 
 function Layout() {
     return (
@@ -15,6 +20,11 @@ function Layout() {
             </div>
             <Routes>
                {/* <Route exact={true} path="/" element={<HomePage />}></Route> */}
+               {/* <Route path="/about" element={<AboutPage />}></Route> */}
+               {/* <Route path="/push-exercises" element={<PushPage />}></Route> */}
+               {/* <Route path="/pull-exercises" element={<PullPage />}></Route> */}
+               {/* <Route path="/hip-dominant-exercises" element={<HipPage />}></Route> */}
+               {/* <Route path="/knee-dominant-exercises" element={<KneePage />}></Route> */}
             </Routes>
         </>
     )
