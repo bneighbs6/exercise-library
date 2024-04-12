331d0f1d55d4549984fbac75eed7ff59867c36cf
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-04-11 21:11:27 -0600

    Added search bar to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,6 +8,7 @@ It will have:
 - Home button
 - About button
 - Dropdown list for types of exercises
+- Search bar
 - More items....
 */
 
@@ -19,21 +20,27 @@ export default function Navbar() {
       <Link className="navbar-item" to="/about">About</Link>
       <div className="dropdown">
         {/* DEBUG: Dropdown menu does not dropdown when pressed */}
-          <button
-            onClick={() => console.log("Dropdown Clicked")}
-            className="btn btn-dark dropdown-toggle"
-            type="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false">
-            Exercise Library
-          </button>
-          <ul className="dropdown-menu dropdown-menu-dark">
-            <li className="dropdown-item">Push Exercises</li>
-            <li className="dropdown-item">Pull Exercises</li>
-            <li className="dropdown-item">Hip Dominant Exercises</li>
-            <li className="dropdown-item">Knee Dominant Exercises</li>
-            <li className="dropdown-item">Trunk Exercises</li>
-          </ul>
+        <button
+          onClick={() => console.log("Dropdown Clicked")}
+          className="btn btn-dark dropdown-toggle"
+          type="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false">
+          Exercise Library
+        </button>
+        <ul className="dropdown-menu dropdown-menu-dark">
+          <li className="dropdown-item">Push Exercises</li>
+          <li className="dropdown-item">Pull Exercises</li>
+          <li className="dropdown-item">Hip Dominant Exercises</li>
+          <li className="dropdown-item">Knee Dominant Exercises</li>
+          <li className="dropdown-item">Trunk Exercises</li>
+        </ul>
+      </div>
+      <div className="search-bar">
+        <form className="d-flex" role="search">
+          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          <button className="btn btn-outline-success" type="submit">Search</button>
+        </form>
       </div>
     </nav>
   );
