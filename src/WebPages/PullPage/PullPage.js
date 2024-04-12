503db1ce20b1cd807e47d1df57c77b79a68d38c7
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-04-12 14:55:37 -0600

    Created a PullPageCard

--- src/WebPages/PullPage/PullPage.js
+++ src/WebPages/PullPage/PullPage.js
@@ -1,5 +1,24 @@
 import React from "react";
+import PullPageCard from "./PullPageCard";
 
 export default function PullPage() {
-    return <h1>Pull Exercises</h1>
+  return (
+    <>
+      <h1>Pull Exercises</h1>
+      <div className="row">
+        <div className="col">
+          <PullPageCard />
+        </div>
+        <div className="col">
+          <PullPageCard />
+        </div>
+        <div className="col">
+          <PullPageCard />
+        </div>
+        <div className="col">
+          <PullPageCard />
+        </div>
+      </div>
+    </>
+  );
 }
