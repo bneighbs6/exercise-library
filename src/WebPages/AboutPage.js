4f547c5f8892228e9f32b1356cfb4e9d4d18cf1e
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-04-17 22:30:46 -0600

    imported react-bootstrap & editted AboutPage to use it

--- src/WebPages/AboutPage.js
+++ src/WebPages/AboutPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Container, Row, Col } from "react-bootstrap";
 /* 
 Page should display a couple of columns describing different parts of the business
 Maybe why you created the exercise library
@@ -7,41 +7,39 @@ How this library differs from others
 */
 
 export default function AboutPage() {
-    return (
-        <>
-            <h1 className="text-center">About Us</h1>
-            <div className="container">
-                <div className="row">
-                    <div className="col text-start">
-                        <h2>Some Bullshit</h2>
-                        <p>
-                            Some more bullshit that means absolutely nothing!
-                            I really just want to test out what this looks like on the
-                            web page.
-                        </p>
-                    </div>
-                    <div className="col text-center">
-                        <h2>Some Bullshit</h2>
-                        <p>
-                            Some more bullshit that means absolutely nothing!
-                            I really just want to test out what this looks like on the
-                            web page.
-                        </p>
-                    </div>
-                    <div className="col text-end">
-                        <h2>Some Bullshit</h2>
-                        <p>
-                            Some more bullshit that means absolutely nothing!
-                            I really just want to test out what this looks like on the
-                            web page.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <img
-                src="https://jackcityfitness.com/wp-content/uploads/10-Essential-Weight-Lifting-Tips.jpg"
-                alt="company logo"
-                className="center-image img-fluid" />
-        </>
-    )
+  return (
+    <>
+      <h1 className="text-center">About Us</h1>
+      <Container>
+        <Row>
+          <Col className=" text-start">
+            <h2>Some Bullshit</h2>
+            <p>
+              Some more bullshit that means absolutely nothing! I really just
+              want to test out what this looks like on the web page.
+            </p>
+          </Col>
+          <Col className="text-center">
+            <h2>Some Bullshit</h2>
+            <p>
+              Some more bullshit that means absolutely nothing! I really just
+              want to test out what this looks like on the web page.
+            </p>
+          </Col>
+          <Col className="text-end">
+            <h2>Some Bullshit</h2>
+            <p>
+              Some more bullshit that means absolutely nothing! I really just
+              want to test out what this looks like on the web page.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+      <img
+        src="https://jackcityfitness.com/wp-content/uploads/10-Essential-Weight-Lifting-Tips.jpg"
+        alt="company logo"
+        className="center-image img-fluid"
+      />
+    </>
+  );
 }
