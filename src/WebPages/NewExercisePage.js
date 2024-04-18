d2f74f86a6d935bbd98fed62e93d55a2c5d1b298
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-04-17 22:27:53 -0600

    Made the NewExercisePage form flex with page size

--- src/WebPages/NewExercisePage.js
+++ src/WebPages/NewExercisePage.js
@@ -1,42 +1,62 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
 
 function NewExercisePage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-/* You need to create an actual submit button */  
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log("Form submitted.")
-        return navigate("/new-exercise");
-    }
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Form submitted.");
+    return navigate("/new-exercise");
+  }
 
-    function handleCancel(e) {
-        console.log("Form cancelled.")
-        return navigate("/new-exercise");
-    }
+  function handleCancel(e) {
+    console.log("Form cancelled.");
+    return navigate("/new-exercise");
+  }
 
-    return (
-        <>
-        <div className="card text-center mx-auto" style={{width: "60rem"}}>
+  return (
+    <Container fluid>
+      <Row className="justify-content-md-center">
+        <Col>
+          <Card className="text-center">
             <div className="card-title">
-                <h1>New Exercise</h1>
+              <h1>New Exercise</h1>
             </div>
-        <form>
-        <div className="form-group" id="exerciseName">
-            <label htmlFor="exerciseNameInput">Exercise Name</label>
-            <input type="text" className="form-control text-center" id="exerciseNameInput" name="exerciseName" placeholder="Exercise name here" />
-        </div>
-        <div className="form-group" id="exercisePattern">
-            <label htmlFor="exercisePatternInput">Exercise Pattern</label>
-            <input type="text" className="form-control text-center" id="exercisePatternInput" name="exerciseType" placeholder="Push, Pull, Hip, Knee, or Core" />
-        </div>
-        <button className="btn btn-success" onClick={handleSubmit}>Submit</button>
-        <button className="btn btn-danger" onClick={handleCancel}>Cancel</button>
-    </form>
-        </div>
-        </>
-    )
+            <Form>
+              <div className="form-group" id="exerciseName">
+                <label htmlFor="exerciseNameInput">Exercise Name</label>
+                <input
+                  type="text"
+                  className="form-control text-center"
+                  id="exerciseNameInput"
+                  name="exerciseName"
+                  placeholder="Exercise name here"
+                />
+              </div>
+              <div className="form-group" id="exercisePattern">
+                <label htmlFor="exercisePatternInput">Exercise Pattern</label>
+                <input
+                  type="text"
+                  className="form-control text-center"
+                  id="exercisePatternInput"
+                  name="exerciseType"
+                  placeholder="Push, Pull, Hip, Knee, or Core"
+                />
+              </div>
+              <Button className="btn-success" onClick={handleSubmit}>
+                Submit
+              </Button>
+              <Button className="btn-danger" onClick={handleCancel}>
+                Cancel
+              </Button>
+            </Form>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default NewExercisePage;
