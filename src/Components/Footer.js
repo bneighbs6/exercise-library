8903020d981ff7296547a2a8cfe080cc97bfbf0e
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-05-11 21:26:56 -0600

    Added lorem ipsum to footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,21 +9,39 @@ function Footer() {
         <div className="row">
           <div className="col-sm-12 col-md-4 text-center">
             <h3>Contact Info</h3>
-            <p>Some bullshit</p>
-            <p>Some more bullshit</p>
-            <p>Some EXTRA BULLSHIT</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
           </div>
           <div className="col-sm-12 col-md-4 text-center">
             <h3>FAQ</h3>
-            <p>Some bullshit</p>
-            <p>Some more bullshit</p>
-            <p>Some EXTRA BULLSHIT</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
           </div>
           <div className="col-sm-12 col-md-4 text-center">
             <h3>About the Creator</h3>
-            <p>Some bullshit</p>
-            <p>Some more bullshit</p>
-            <p>Some EXTRA BULLSHIT</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
           </div>
         </div>
       </footer>
