becb9de89a7b0af3f46505d223daee748cb18af2
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-08-14 21:00:59 -0600

    Made fake middleware fx to practice

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,20 @@
 // Require Express package and assign to variable
 const express = require("express");
 
+// Require Morgan package
+// Morgan will print useful info to terminal window on each request
+const morgan = require("morgan");
+
+const sayHello = (req, res, next) => {
+    res.send("Hello!");
+    next(); 
+}
+
 // Express package exports a function, when invoked, a new Express app is created and assigned to a variable
 const app = express();
+
+app.use(morgan("dev"));
+app.use(sayHello);
 
 // Export Express app to be used in other files
 module.exports = app; 
