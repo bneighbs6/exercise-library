518f2c14d7bf5b2d44c2f32274ee74f6f9a103b5
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-08-14 21:27:17 -0600

    Messing with query parameters

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,11 +10,21 @@ const sayHello = (req, res, next) => {
     next(); 
 }
 
+// Logs the req.query object to the console
+// If name query parameter exists, it sends the first message, otherwise send the second message
+const saySomethingCool = (req, res, next) => {
+    console.log(req.query);
+    const name = req.query.name;
+    let message = name ? `Sup ${name}?` : "I'm cool, dawg";
+    res.send(message);
+}
+
 // Express package exports a function, when invoked, a new Express app is created and assigned to a variable
 const app = express();
 
 app.use(morgan("dev"));
 app.get("/hello", sayHello);
+app.get("/cool", saySomethingCool);
 
 // Export Express app to be used in other files
 module.exports = app; 
