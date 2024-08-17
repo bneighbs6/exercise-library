cd133d1cd58fecd664001670ff6bc82cceb04f3a
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-08-16 22:00:30 -0600

    Create ability to view exercise by its id

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,9 +8,20 @@ const morgan = require("morgan");
 // Exercise data from data folder
 const exercises = require("./data/exercise-data");
 
-// View exercise data in json format
+// View exercise data in json format; used with GET /exercises
 const viewExercises = (req, res, next) => {
     res.json({ data: exercises });
+}
+
+const viewExerciseById = (req, res, next) => {
+    const exerciseId = req.params.exerciseId; 
+    const foundExercise = exercises.find((exercise) => exercise.id === Number(exerciseId));
+
+    if (foundExercise) {
+        res.json({ data: foundExercise });
+    } else {
+        next(`Exercise ID not found: ${exerciseId}`);
+    }
 }
 
 // Express package exports a function, when invoked, a new Express app is created and assigned to a variable
@@ -19,8 +30,11 @@ const app = express();
 // Route set up 
 app.use(morgan("dev"));
 
+// View exercise data in json format by Exercise Id
+app.use("/exercises/:exerciseId", viewExerciseById);
+
 // View exercise data in json format
-app.get("/exercises", viewExercises);
+app.use("/exercises", viewExercises);
 
 // Route not found handler
 app.use((req, res, next) => {
