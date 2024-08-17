46b8fc4dc9e47c4984cfcea2509bf92f04b0dc2e
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-08-16 21:51:56 -0600

    Removed all "messing around" code

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,48 +8,7 @@ const morgan = require("morgan");
 // Exercise data from data folder
 const exercises = require("./data/exercise-data");
 
-// const sayHello = (req, res, next) => {
-//     res.send("Hello!");
-//     next(); 
-// }
-
-// Logs the req.query object to the console
-// If name query parameter exists, it sends the first message, otherwise send the second message
-// const saySomethingCool = (req, res, next) => {
-//     console.log(req.query);
-//     const name = req.query.name;
-//     let message = name ? `Sup ${name}?` : "I'm cool, dawg";
-//     res.send(message);
-// }
-
-// Build a route using route parameters
-// const sayGreeting = (req, res, next) => {
-//     console.log(req.params);
-//     const greeting = req.params.greeting; 
-//     const content = `${greeting}!`;
-//     res.send(content)
-// }
-
-// const useBothQueryAndParameter = (req, res, next) => {
-//     console.log(req.params);
-//     console.log(req.query);
-
-//     const sport = req.params.sport;
-//     const athlete = req.query.athlete; 
-
-//     const content = athlete ? `${sport}, ${athlete}` : "You need to enter either a query or a parameter.";
-//     res.send(content)
-// }
-
-// const throwError = (req, res, next) => {
-//     const exercise = req.params.exercise; 
-//     if (!exercise) {
-//         next("No exercise parameter was given.")
-//     } else {
-//         res.send(`${exercise} is a great exercise`);
-//     }
-// }
-
+// View exercise data in json format
 const viewExercises = (req, res, next) => {
     res.json({ data: exercises });
 }
@@ -59,16 +18,6 @@ const app = express();
 
 // Route set up 
 app.use(morgan("dev"));
-// app.get("/hello", sayHello);
-// app.get("/cool", saySomethingCool);
-// Route using route parameters to edit what is sent on the response
-// app.get("/say/:greeting", sayGreeting);
-
-// Route using both query and parameters
-// http://localhost:8000/both/football/?athlete=elliot will produce => football, elliot
-// app.get("/both/:sport", useBothQueryAndParameter);
-
-// app.get("/exercise/:exercise", throwError);
 
 // View exercise data in json format
 app.get("/exercises", viewExercises);
