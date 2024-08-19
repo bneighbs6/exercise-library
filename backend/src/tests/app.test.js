a9f5085e73cbb3e17f687b083498a92bb07ccf71
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-08-19 11:23:31 -0600

    Basic setup for test file

---
+++ backend/src/tests/app.test.js
@@ -0,0 +1,11 @@
+// Load supertest library
+const request = require("supertest");
+// Load exercises data
+const exercises = require("../data/exercise-data");
+// Load express server
+const app = require("../app");
+
+describe("path /exercises", () => {
+    // Add tests here
+});
+
