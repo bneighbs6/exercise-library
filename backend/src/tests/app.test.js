c4b46fa540a492cf41485e6d0a3074778988343a
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-08-19 12:10:44 -0600

    Set up 2 basic tests for GET /exercises

--- backend/src/tests/app.test.js
+++ backend/src/tests/app.test.js
@@ -6,6 +6,48 @@ const exercises = require("../data/exercise-data");
 const app = require("../app");
 
 describe("path /exercises", () => {
-    // Add tests here
+    // beforeEach() will clear out exercises data
+    beforeEach(() => {
+        exercises.splice(0, exercises.length); // Clears out exercises data
+    });
+
+    // Testing GET /exercises endpoint
+    describe("GET method", () => {
+        it ("returns a 200 status code", async () => {
+            const response = await request(app).get("/exercises");
+
+            expect(response.status).toBe(200);
+        });
+
+        it ("responds with an array of exercises", async () => {
+            const expected = [
+                {
+                    id: 1,
+                    category: "push",
+                    name: "Push Up",
+                },
+                {
+                    id: 2, 
+                    category: "pull",
+                    name: "Pull Up",
+                },
+                {
+                    id: 3, 
+                    category: "knee",
+                    name: "Squat",
+                },
+                {
+                    id: 4, 
+                    category: "hip",
+                    name: "Deadlift",
+                }
+            ];
+
+            exercises.push(...expected);
+
+            const response = await request(app).get("/exercises");
+            expect(response.body.data).toEqual(expected);
+        })
+    });
 });
 
