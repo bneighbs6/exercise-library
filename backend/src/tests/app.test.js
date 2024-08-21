a60e0d568e15b1ef99b96aa2362eaad2e54f40f1
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-08-20 21:59:03 -0600

    Reorganized app.test.js correctly describes tests

--- backend/src/tests/app.test.js
+++ backend/src/tests/app.test.js
@@ -6,139 +6,132 @@ const exercises = require("../data/exercise-data");
 const app = require("../app");
 
 describe("path /exercises", () => {
-    // beforeEach() will clear out exercises data
-    beforeEach(() => {
-        exercises.splice(0, exercises.length); // Clears out exercises data
+  // beforeEach() will clear out exercises data
+  beforeEach(() => {
+    exercises.splice(0, exercises.length); // Clears out exercises data
+  });
+
+  // Testing GET /exercises endpoint
+  describe("GET method", () => {
+    it("returns a 200 status code", async () => {
+      const response = await request(app).get("/exercises");
+
+      expect(response.status).toBe(200);
     });
 
-    // Testing GET /exercises endpoint
-    describe("GET method", () => {
-        it ("returns a 200 status code", async () => {
-            const response = await request(app).get("/exercises");
+    it("responds with an array of exercises", async () => {
+      const expected = [
+        {
+          id: 1,
+          category: "push",
+          name: "Push Up",
+        },
+        {
+          id: 2,
+          category: "pull",
+          name: "Pull Up",
+        },
+        {
+          id: 3,
+          category: "knee",
+          name: "Squat",
+        },
+        {
+          id: 4,
+          category: "hip",
+          name: "Deadlift",
+        },
+      ];
 
-            expect(response.status).toBe(200);
-        });
+      exercises.push(...expected);
 
-        it ("responds with an array of exercises", async () => {
-            const expected = [
-                {
-                    id: 1,
-                    category: "push",
-                    name: "Push Up",
-                },
-                {
-                    id: 2, 
-                    category: "pull",
-                    name: "Pull Up",
-                },
-                {
-                    id: 3, 
-                    category: "knee",
-                    name: "Squat",
-                },
-                {
-                    id: 4, 
-                    category: "hip",
-                    name: "Deadlift",
-                }
-            ];
+      const response = await request(app).get("/exercises");
+      expect(response.body.data).toEqual(expected);
+    });
+  });
 
-            exercises.push(...expected);
+  // Test POST /exercises endpoint
+  describe("POST method", () => {
+    it("creates a new exercise and assigns id", async () => {
+      const newExercise = {
+        category: "pull",
+        name: "3 point row",
+      };
+      const response = await request(app)
+        .post("/exercises")
+        .set("Accept", "application/json")
+        .send({ data: newExercise });
 
-            const response = await request(app).get("/exercises");
-            expect(response.body.data).toEqual(expected);
-        })
+      expect(response.status).toBe(201);
+      expect(response.body.data).toEqual({
+        id: 5,
+        ...newExercise,
+      });
     });
 
-    // Test POST /exercises endpoint
-    describe("POST method", () => {
-        it("creates a new exercise and assigns id", async () => {
-            const newExercise = {
-                category: "pull",
-                name: "3 point row",
-            };
-            const response = await request(app)
-            .post("/exercises")
-            .set("Accept", "application/json")
-            .send({ data: newExercise });
+    it("returns 400 if name is missing", async () => {
+      const response = await request(app)
+        .post("/exercises")
+        .set("Accept", "application/json")
+        .send({ data: { category: "push" } });
 
-            expect(response.status).toBe(201);
-            expect(response.body.data).toEqual({
-                id: 5,
-                ...newExercise,
-            });
-        });
-
-        it("returns 400 if name is missing", async () => {
-            const response = await request(app)
-            .post("/exercises")
-            .set("Accept", "application/json")
-            .send({ data: { category: "push" } });
-
-            expect(response.status).toBe(400);
-        });
-
-        it("returns 400 if name is empty", async () => {
-            const response = await request(app)
-            .post("/exercises")
-            .set("Accept", "application/json")
-            .send({ data: { category: "push", name: "" } });
-
-            expect(response.status).toBe(400);
-        });
+      expect(response.status).toBe(400);
     });
 
-    // Test PUT /:exerciseId endpoint
-    describe("PUT method", () => {
-        beforeEach(() => {
-            exercises.splice(0, exercises.length); // Clears out exercises data
-            exercises.push({ id: 1, name: "Push Up", category: "push" }); // Add a new exercise
-          });
+    it("returns 400 if name is empty", async () => {
+      const response = await request(app)
+        .post("/exercises")
+        .set("Accept", "application/json")
+        .send({ data: { category: "push", name: "" } });
 
-        it("returns 200 when exercise is updated", async () => {
-            // // Create new exercise and add to exercises array
-            // const newExercise = {
-            //     category: "pull",
-            //     name: "3 point row",
-            // };
-            // exercises.push({ id: 5, ...newExercise });
-
-            console.log(exercises[0])
-            // Update new exercise
-            const updatedExercise = {
-                name: "3 Point Row - Updated",
-                category: "Pull",
-            };
-
-            // Send PUT Request
-            const response = await request(app)
-            .put(`/exercises/${exercises[0].id}`)
-            .set("Accept", "application/json")
-            .send({ data: updatedExercise });
-
-            // Expect response status to be 200
-            expect(response.status).toBe(200);
-        });
-
-        it("returns 404 if exerciseId does not exist", async () => {
-            // Send PUT request with an invalid id
-            const response = await request(app)
-            .put(`/exercises/1000001`)
-            .set("Accept", "application/json")
-            .send({ data: { category: "Pull", name: "Chin up" } });
-
-            // Expect response status to be 404
-            expect(response.status).toBe(404);
-        });
-
-        it("returns 400 if a body data is invalid", async () => {
-            const response = await request(app)
-            .put(`/exercises/${exercises[0].id}`)
-            .set("Accept", "application/json")
-            .send({ data: { category: "", name: "Squat" } });
-
-            expect(response.status).toBe(400);
-        });
+      expect(response.status).toBe(400);
     });
+  });
 });
 
+describe("path /:exerciseId", () => {
+  // Test PUT /:exerciseId endpoint
+  describe("PUT method", () => {
+    beforeEach(() => {
+      exercises.splice(0, exercises.length); // Clears out exercises data
+      exercises.push({ id: 1, name: "Push Up", category: "push" }); // Add a new exercise
+    });
+
+    it("returns 200 when exercise is updated", async () => {
+      // Update new exercise
+      const updatedExercise = {
+        name: "3 Point Row - Updated",
+        category: "Pull",
+      };
+
+      // Send PUT Request
+      const response = await request(app)
+        .put(`/exercises/${exercises[0].id}`)
+        .set("Accept", "application/json")
+        .send({ data: updatedExercise });
+
+      // Expect response status to be 200
+      expect(response.status).toBe(200);
+    });
+
+    it("returns 404 if exerciseId does not exist", async () => {
+      // Send PUT request with an invalid id
+      const response = await request(app)
+        .put(`/exercises/1000001`)
+        .set("Accept", "application/json")
+        .send({ data: { category: "Pull", name: "Chin up" } });
+
+      // Expect response status to be 404
+      expect(response.status).toBe(404);
+    });
+
+    it("returns 400 if a body data is invalid", async () => {
+      const response = await request(app)
+        .put(`/exercises/${exercises[0].id}`)
+        .set("Accept", "application/json")
+        .send({ data: { category: "", name: "Squat" } });
+
+      expect(response.status).toBe(400);
+    });
+  });
+});
