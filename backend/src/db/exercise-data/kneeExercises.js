8f555fea2d4c0a55716882b1ec1b004ae5a72afc
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-08-25 14:01:13 -0600

    Seeded knee exercise data

--- backend/src/db/exercise-data/kneeExercises.js
+++ backend/src/db/exercise-data/kneeExercises.js
@@ -5,4 +5,19 @@ module.exports = [
     {
         exercise_name: "Single Leg Squat"
     },
+    {
+        exercise_name: "Split Squat"
+    },
+    {
+        exercise_name: "RFE Split Squat"
+    },
+    {
+        exercise_name: "FFE Split Squat"
+    },
+    {
+        exercise_name: "Reverse Lunge"
+    },
+    {
+        exercise_name: "Forward Lunge"
+    },
 ]
