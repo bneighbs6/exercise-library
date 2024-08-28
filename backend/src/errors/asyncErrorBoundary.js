8886e8ceef86774ce3eb1f2b00dd06451ee3200c
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-08-27 21:38:19 -0600

    Updated async error boundary.

--- backend/src/errors/asyncErrorBoundary.js
+++ backend/src/errors/asyncErrorBoundary.js
@@ -1,12 +1,19 @@
-function asyncErrorBoundary(delegate, defaultStatus = 500) {
-  return (req, res, next) => {
-      Promise.resolve()
-          .then(() => delegate(req, res, next))
-          .catch((error = {}) => {
-              console.error('Error occurred:', error);
-              const { status = defaultStatus, message = 'An unexpected error occurred' } = error;
-              next({ status, message });
-          });
+// asyncErrorBoundary() will wrap around route handlers/middleware functions and handle the async errors should they come up.
+
+// Delegate parameter is an async/await handler/middleware function that will be called by asyncErrorBoundary
+function asyncErrorBoundary(delegate, defaultStatus) {
+  // Returns an Express handler or middleware function that will be called by Express in place of the delegate function
+  return (request, response, next) => {
+    Promise.resolve()
+      .then(() => delegate(request, response, next))
+      .catch((error = {}) => {
+        console.log("An error has occured:", error);
+        const { status = defaultStatus, message = error } = error;
+        next({
+          status,
+          message,
+        });
+      });
   };
 }
 
