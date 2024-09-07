7e099c4584bca175b0a3daec3101d650e87f34ed
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-09-07 13:02:01 -0600

    Updated production & staging configs

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -19,36 +19,26 @@ module.exports = {
     }
   },
 
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
+  staging: {
+    client: 'pg',
+    connection: DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"), // Tells knex where to store migration files
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"), // Tells knex where to store seeds files
+    }
+  },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  production: {
+    client: 'pg',
+    connection: DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"), // Tells knex where to store migration files
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"), // Tells knex where to store seeds files
+    }
+  }
 
 };
