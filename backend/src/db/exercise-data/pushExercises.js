5890d9d1e1e0a15e23fa302be1d3e11692c04f00
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-09-07 21:17:03 -0600

    Added some data for pushExercises

---
+++ backend/src/db/exercise-data/pushExercises.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        exercise_name: "Push Ups"
+    }, 
+    {
+        exercise_name: "Bench Press"
+    },
+    {
+        exercise_name: "Overhead Press"
+    },
+    {
+        exercise_name: "Incline Bench Press"
+    },
+    {
+        exercise_name: "Front Raise"
+    },
+]
