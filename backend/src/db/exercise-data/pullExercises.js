a3860cf003e35e1dc7e7de25125ceddafc8c495f
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-09-09 17:41:00 -0600

    Added data to pullExercises.js

---
+++ backend/src/db/exercise-data/pullExercises.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        exercise_name: "Pull Ups",
+    },
+    {
+        exercise_name: "Chin Ups",
+    },
+    {
+        exercise_name: "Lat Pulldown",
+    },
+    {
+        exercise_name: "3 Point Row",
+    },
+]
