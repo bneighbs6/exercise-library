ad6e433f95602d26f6967435bc445dcdb9a19913
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-09-09 17:36:11 -0600

    Prettier code

--- backend/src/exercises/pushExercises/pushExercises.controller.js
+++ backend/src/exercises/pushExercises/pushExercises.controller.js
@@ -5,16 +5,16 @@ const asyncErrorBoundary = require("../../errors/asyncErrorBoundary");
 /* VALIDATION MIDDLEWARE */
 
 function bodyHasData(propertyName) {
-    return function (req, res, next) {
-        const { data: { exercise_name } = {} } = req.body; 
-        if (exercise_name) {
-            return next();
-        }
-        next({
-            status: 400, 
-            message: `A ${propertyName} is required.`
-        });
+  return function (req, res, next) {
+    const { data: { exercise_name } = {} } = req.body;
+    if (exercise_name) {
+      return next();
     }
+    next({
+      status: 400,
+      message: `A ${propertyName} is required.`,
+    });
+  };
 }
 
 // Validates that exercise exists by its id
@@ -32,54 +32,65 @@ async function exerciseExists(req, res, next) {
 
 // Validates that the category is a valid value
 function categoryHasValidValue(req, res, next) {
-    const { data: { exercise_category } = {} } = req.body; 
-    const validCategory = ["Push", "Pull", "Hip", "Knee"];
-    if (validCategory.includes(exercise_category)) {
-        return next(); 
-    } else {
-        next({
-            status: 400,
-            message: `Value of 'category' property must be one of ${validCategory}. Received ${category}`,
-        });
-    }
+  const { data: { exercise_category } = {} } = req.body;
+  const validCategory = ["Push", "Pull", "Hip", "Knee"];
+  if (validCategory.includes(exercise_category)) {
+    return next();
+  } else {
+    next({
+      status: 400,
+      message: `Value of 'category' property must be one of ${validCategory}. Received ${category}`,
+    });
+  }
 }
 
 /* CRUDL Functions */
 
 async function create(req, res, next) {
-    const data = await service.create(req.body.data);
-    res.status(201).json({ data })
+  const data = await service.create(req.body.data);
+  res.status(201).json({ data });
 }
 
 function read(req, res, next) {
-    const { exercise: data } = res.locals; 
-    res.json({ data });
+  const { exercise: data } = res.locals;
+  res.json({ data });
 }
 
 async function update(req, res, next) {
-    const updatedExercise = {
-        ...req.body.data,
-        exercise_id: res.locals.exercise.exercise_id,
-    };
-    const data = await service.update(updatedExercise);
-    res.json({ data });
+  const updatedExercise = {
+    ...req.body.data,
+    exercise_id: res.locals.exercise.exercise_id,
+  };
+  const data = await service.update(updatedExercise);
+  res.json({ data });
 }
 
 async function destroy(req, res, next) {
-    const { exercise } = res.locals; 
-    await service.delete(exercise.exercise_id);
-    res.sendStatus(204);
+  const { exercise } = res.locals;
+  await service.delete(exercise.exercise_id);
+  res.sendStatus(204);
 }
 
 async function list(req, res, next) {
-    const data = await service.list(); 
-    res.json({ data });
+  const data = await service.list();
+  res.json({ data });
 }
 
 module.exports = {
-    create: [bodyHasData("exercise_category"), bodyHasData("exercise_name"), categoryHasValidValue, asyncErrorBoundary(create)],
-    read: [asyncErrorBoundary(exerciseExists), read],
-    update: [asyncErrorBoundary(exerciseExists), bodyHasData("exercise_category"), bodyHasData("exercise_name"), categoryHasValidValue, asyncErrorBoundary(update)],
-    delete: [asyncErrorBoundary(exerciseExists), asyncErrorBoundary(destroy)],
-    list: asyncErrorBoundary(list),
-}
+  create: [
+    bodyHasData("exercise_category"),
+    bodyHasData("exercise_name"),
+    categoryHasValidValue,
+    asyncErrorBoundary(create),
+  ],
+  read: [asyncErrorBoundary(exerciseExists), read],
+  update: [
+    asyncErrorBoundary(exerciseExists),
+    bodyHasData("exercise_category"),
+    bodyHasData("exercise_name"),
+    categoryHasValidValue,
+    asyncErrorBoundary(update),
+  ],
+  delete: [asyncErrorBoundary(exerciseExists), asyncErrorBoundary(destroy)],
+  list: asyncErrorBoundary(list),
+};
