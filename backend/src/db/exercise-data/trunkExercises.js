b7335bf21497cf13cafcb0924a461b807e891c8d
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-09-09 21:04:47 -0600

    created trunk exercise data

---
+++ backend/src/db/exercise-data/trunkExercises.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        exercise_name: "Plank",
+    },
+    {
+        exercise_name: "Side Plank",
+    },
+    {
+        exercise_name: "Rollouts",
+    },
+    {
+        exercise_name: "KB Pull Thrus",
+    },
+]
