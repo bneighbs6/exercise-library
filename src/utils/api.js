f5a2c6684773ffd64d93fc478ac0bd96c0720370
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-09-09 21:30:43 -0600

    Created createExercise to send POST to API

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,6 @@
-const API_BASE_URL = 
-    process.env.REACT_APP_API_BASE_URL
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
-const headers = new Headers(); 
+const headers = new Headers();
 headers.append("Content-Type", "application/json");
 
 /**
@@ -20,37 +19,37 @@ headers.append("Content-Type", "application/json");
  *  If the response is not in the 200 - 399 range the promise is rejected.
  */
 async function fetchJson(url, options, onCancel) {
-    try {
-      const response = await fetch(url, options);
-  
-      if (response.status === 204) {
-        return null;
-      }
-  
-      const payload = await response.json();
-  
-      if (payload.error) {
-        return Promise.reject({ message: payload.error });
-      }
-      return payload.data;
-    } catch (error) {
-      if (error.name !== "AbortError") {
-        console.error(error.stack);
-        throw error;
-      }
-      return Promise.resolve(onCancel);
-    }
-  }
+  try {
+    const response = await fetch(url, options);
 
-// Create an exercise and send data to the API 
-async function createExercise(exercise, signal) {
-    // Send a POST method to the API with exercise information
-    const url = `${API_BASE_URL}/new-exercise`;
-    const options = {
-        method: "POST",
-        headers,
-        body: JSON.stringify({ data: exercise }),
-        signal,
+    if (response.status === 204) {
+      return null;
     }
-    return await fetchJson(url, options, {});
+
+    const payload = await response.json();
+
+    if (payload.error) {
+      return Promise.reject({ message: payload.error });
+    }
+    return payload.data;
+  } catch (error) {
+    if (error.name !== "AbortError") {
+      console.error(error.stack);
+      throw error;
+    }
+    return Promise.resolve(onCancel);
+  }
+}
+
+// Create an exercise and send data to the API
+export async function createExercise(exercise, signal) {
+  // Send a POST method to the API with exercise information
+  const url = `${API_BASE_URL}/new-exercise`;
+  const options = {
+    method: "POST",
+    headers,
+    body: JSON.stringify({ data: exercise }),
+    signal,
+  };
+  return await fetchJson(url, options, {});
 }
