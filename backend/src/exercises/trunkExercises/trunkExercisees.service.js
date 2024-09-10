f168d73daa128f5470824a34362665ca45c8caed
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-09-09 21:18:16 -0600

    Created service fx for trunk exercises

--- backend/src/exercises/trunkExercises/trunkExercisees.service.js
+++ backend/src/exercises/trunkExercises/trunkExercisees.service.js
@@ -1,1 +1,45 @@
 // TODO: Create service functions
+
+const knex = require("../../db/connection");
+
+// Creates a new trunk exercise
+function create(exercise) {
+    return knex("trunk_exercises")
+    .insert(exercise)
+    .returning("*")
+    .then((createdExercise) => createdExercise[0]);
+}
+
+// Reads a trunk exercise by its id
+function read(exerciseId) {
+    return knex("trunk_exercises")
+    .select("*")
+    .where({ exercise_id: exerciseId })
+    .first(); 
+}
+
+// Updates an exercise by its id
+function update(udatedExercise) {
+    return knex("trunk_exercises")
+    .select("*")
+    .where({ exercise_id: updatedExercise.exercise_id })
+    .update(updatedExercise, "*");
+}
+
+// Deletes an exercise by its id
+function destroy(exercise_id) {
+    return knex("trunk_exercises").where({ exercise_id }).del(); 
+}
+
+// Selects all columns from trunk_exercises table
+function list() {
+    return knex("trunk_exercises").select("*");
+}
+
+module.exports = {
+    create,
+    read,
+    update,
+    delete: destroy,
+    list, 
+}
