6b4cce777b9ad2e3644e1e64aaaecd218aa84738
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-09-09 19:01:31 -0600

    Created CRUDL fx in pullExercises.controller

--- backend/src/exercises/pullExercises/pullExercises.controller.js
+++ backend/src/exercises/pullExercises/pullExercises.controller.js
@@ -42,3 +42,56 @@ function categoryHasValidValue(req, res, next) {
     });
   }
 }
+
+/* CRUDL Functions */
+
+async function create(req, res, next) {
+    const data = await service.create(req.body.data);
+    res.status(201).json({ data });
+}
+
+function read(req, res, next) {
+    const { exercise: data } = res.locals; 
+    res.json({ data });
+}
+
+async function update(req, res, next) {
+    const updatedExercise = {
+        ...req.body.data, 
+        exercise_id: res.locals.exercise.exercisd_id,    
+    };
+
+    const data = await service.update(updatedExercise);
+    res.json({ data });
+}
+
+async function destroy(req, res, next) {
+    const { exercise } = res.locals; 
+    await service.delete(exercise.exercisd_id);
+    res.sendStatus(204);
+}
+
+async function list(req, res, next) {
+    const data = await service.list();
+    res.json({ data });
+}
+
+module.exports = {
+    create: [
+      bodyHasData("exercise_category"),
+      bodyHasData("exercise_name"),
+      categoryHasValidValue,
+      asyncErrorBoundary(create),
+    ],
+    read: [asyncErrorBoundary(exerciseExists), read],
+    update: [
+      asyncErrorBoundary(exerciseExists),
+      bodyHasData("exercise_category"),
+      bodyHasData("exercise_name"),
+      categoryHasValidValue,
+      asyncErrorBoundary(update),
+    ],
+    delete: [asyncErrorBoundary(exerciseExists), asyncErrorBoundary(destroy)],
+    list: asyncErrorBoundary(list),
+  };
+ 
