a971483cd368663e597865f9611abb86120ef8f0
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-09-09 19:03:36 -0600

    Set up router for pullExercises

--- backend/src/exercises/pullExercises/pullExercises.router.js
+++ backend/src/exercises/pullExercises/pullExercises.router.js
@@ -1,9 +1,16 @@
 const router = require("express").Router();
+const methodNotAllowed = require("../../errors/methodNotAllowed");
+const controller = require("./pullExercises.controller");
 
-const exercises = require("../../db/exercise-data/pullExercises");
+router.route("/:exerciseId([0-9]+)")
+.get(controller.read)
+.put(controller.update)
+.delete(controller.delete)
+.all(methodNotAllowed);
 
-router.route("/").get((req, res, next) => {
-    res.json({ data: exercises });
-});
+router.route("/")
+.get(controller.list)
+.post(controller.create)
+.all(methodNotAllowed);
 
 module.exports = router; 
