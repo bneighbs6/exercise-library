42f5ac2208e9d268bc7e80564ac553046c2927d9
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-09-20 11:26:33 -0600

    Displays "Loading..." when loading === true

--- src/WebPages/KneePage/KneePage.js
+++ src/WebPages/KneePage/KneePage.js
@@ -1,4 +1,3 @@
-// KneePage.js
 import React, { useEffect, useState } from "react";
 import KneePageCard from "./KneePageCard";
 import { Row, Col } from "react-bootstrap";
@@ -9,39 +8,43 @@ export default function KneePage() {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
-useEffect(() => {
-  async function loadExercises() {
-    setLoading(true);
-    try {
-      const response = await fetch("https://exercise-library-backend.onrender.com/knee-exercises");
-      const exercisesFromAPI = await response.json(); 
-      console.log(exercisesFromAPI);
-      setExercises(exercisesFromAPI.data);
-      setLoading(false);
-    } catch (error) {
-      setError(error);
-      setLoading(false);
+  useEffect(() => {
+    async function loadExercises() {
+      setLoading(true);
+      try {
+        const response = await fetch("https://exercise-library-backend.onrender.com/knee-exercises");
+        const exercisesFromAPI = await response.json(); 
+        console.log(exercisesFromAPI);
+        setExercises(exercisesFromAPI.data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
     }
-  }
-  loadExercises(); 
-}, []);
+    loadExercises(); 
+  }, []);
 
   return (
     <>
       <h1 className="text-center">Knee Exercises</h1>
-      <Row>
-      {exercises.length > 0 ? (
-          exercises.map(exercise => (
-            <Col key={exercise.exercise_id} xs={12} md={4} lg={3}>
-              <KneePageCard exercise={exercise} />
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <Row>
+          {exercises.length > 0 ? (
+            exercises.map(exercise => (
+              <Col key={exercise.exercise_id} xs={12} md={4} lg={3}>
+                <KneePageCard exercise={exercise} />
+              </Col>
+            ))
+          ) : (
+            <Col xs={12}>
+              <p>No exercises found.</p>
             </Col>
-          ))
-        ) : (
-          <Col xs={12}>
-            <p>No exercises found.</p>
-          </Col>
-        )}
-      </Row>
+          )}
+        </Row>
+      )}
       {error && <div>Error: {error.message}</div>}
     </>
   );
