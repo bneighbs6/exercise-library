d8fd6f736c38a8f167f9d673052ee02af17a83a5
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-09-21 10:08:30 -0600

    Fetched & displayed all hip exercises

--- src/WebPages/HipPage/HipPage.js
+++ src/WebPages/HipPage/HipPage.js
@@ -1,25 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HipPageCard from "./HipPageCard";
 import { Row, Col } from "react-bootstrap";
 
 function HipPage() {
+const [exercises, setExercises] = useState([]);
+const [error, setError] = useState(null);
+const [loading, setLoading] = useState(false);
+
+useEffect(() => {
+  async function loadExercises()  {
+    setLoading(true);
+    try {
+      const response = await fetch("https://exercise-library-backend.onrender.com/hip-exercises");
+      const exercisesFromAPI = await response.json();
+      setExercises(exercisesFromAPI.data);
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
+  }
+  loadExercises(); 
+}, []);
+
   return (
     <>
       <h1 className="text-center">Hip Exercises</h1>
-      <Row>
-        <Col>
-          <HipPageCard />
-        </Col>
-        <Col>
-          <HipPageCard />
-        </Col>
-        <Col>
-          <HipPageCard />
-        </Col>
-        <Col>
-          <HipPageCard />
-        </Col>
-      </Row>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <Row>
+          {exercises.length > 0 ? (
+            exercises.map((exercise) => (
+              <Col key={exercise.exercise_id} xs={12} md={4} lg={3}>
+                <HipPageCard exercise={exercise} />
+              </Col>
+            ))
+          ) : (
+            <Col xs={12}>
+              <p>No exercises found.</p>
+            </Col>
+          )}
+        </Row>
+      )}
+      {error && <div>Error: {error.message}</div>}
     </>
   );
 }
--- src/WebPages/HipPage/HipPageCard.js
+++ src/WebPages/HipPage/HipPageCard.js
@@ -8,13 +8,13 @@ a text,
 and a button to go directly to that exercise's web page 
 */
 
-function HipPageCard() {
+function HipPageCard({ exercise }) {
   return (
     <>
       <Card className="mx-auto my-3" style={{ width: "18rem", backgroundColor: "black", color: "white" }}>
         <img className="card-img-top" src="..." alt="Hip exercise" />
         <div className="card-body">
-          <h5 className="card-title">Hip Page Card</h5>
+          <h5 className="card-title">{exercise.exercise_name}</h5>
           <p className="card-text">
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
