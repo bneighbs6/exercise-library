9bb89f4eaaa2bd377f85a87fd2b25eeff51d6812
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-09-22 19:42:14 -0600

    Fetched & displayed all push exercises

--- src/WebPages/PushPage/PushPage.js
+++ src/WebPages/PushPage/PushPage.js
@@ -1,25 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PushPageCard from "./PushPageCard";
 import { Row, Col } from "react-bootstrap";
 
 export default function PushPage() {
+
+  const [exercises, setExercises] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function loadExercises() {
+      setLoading(true);
+      try {
+        const response = await fetch("https://exercise-library-backend.onrender.com/push-exercises");
+        const exercisesFromAPI = await response.json(); 
+        setExercises(exercisesFromAPI.data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    }
+    loadExercises();
+  }, []);
+
   return (
     <>
-      <h1 className="text-center">Push Exercises</h1>
-      <Row>
-        <Col>
-          <PushPageCard />
-        </Col>
-        <Col>
-          <PushPageCard />
-        </Col>
-        <Col>
-          <PushPageCard />
-        </Col>
-        <Col>
-          <PushPageCard />
-        </Col>
-      </Row>
+    <h1 className="text-center">Push Exercises</h1>
+    {loading ? (
+        <h1>Loading Push Exercises...</h1>
+    ) : (
+        <Row>
+            {exercises.length > 0 ? (
+                exercises.map((exercise) => (
+                    <Col key={exercises.exercise_id} xs={12} md={4} lg={3}>
+                        <PushPageCard exercise={exercise} />
+                    </Col>
+                ))
+            ) : (
+                <Col xs={12}>
+                    <p>No exercises found.</p>
+                </Col>
+            )}
+        </Row>
+    )}
+    {error && <div>Error: {error.message}</div>}
     </>
-  );
+)
 }
--- src/WebPages/PushPage/PushPageCard.js
+++ src/WebPages/PushPage/PushPageCard.js
@@ -8,12 +8,12 @@ a text,
 and a button to go directly to that exercise's web page 
 */
 
-function PushPageCard() {
+function PushPageCard({ exercise }) {
   return (
     <Card className="mx-auto my-3" style={{ width: "18rem", backgroundColor: "black", color: "white" }}>
-      <img className="card-img-top" src="..." alt="Push exercise" />
+      <img className="card-img-top" src="..." alt={exercise.exercise_name} />
       <div className="card-body">
-        <h5 className="card-title">Push Page Card</h5>
+        <h5 className="card-title">{exercise.exercise_name}</h5>
         <p className="card-text">
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
