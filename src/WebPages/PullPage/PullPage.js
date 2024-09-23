245656f45f44d3c9bb80b3fb28c55c928acc90d1
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-09-22 19:36:47 -0600

    Fetched & displayed pull exercises

--- src/WebPages/PullPage/PullPage.js
+++ src/WebPages/PullPage/PullPage.js
@@ -1,27 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PullPageCard from "./PullPageCard";
 import { Row, Col } from "react-bootstrap";
 
 function PullPage() {
+  const [exercises, setExercises] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function loadExercises() {
+      setLoading(true);
+      try {
+        const response = await fetch("https://exercise-library-backend.onrender.com/pull-exercises");
+        const exercisesFromAPI = await response.json(); 
+        setExercises(exercisesFromAPI.data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    }
+    loadExercises()
+  }, []);
+
   return (
     <>
-      <h1 className="text-center">Pull Exercises</h1>
-      <Row>
-        <Col>
-          <PullPageCard />
-        </Col>
-        <Col>
-          <PullPageCard />
-        </Col>
-        <Col>
-          <PullPageCard />
-        </Col>
-        <Col>
-          <PullPageCard />
-        </Col>
-      </Row>
+    <h1 className="text-center">Pull Exercises</h1>
+    {loading ? (
+        <h1>Loading Trunk Exercises...</h1>
+    ) : (
+        <Row>
+            {exercises.length > 0 ? (
+                exercises.map((exercise) => (
+                    <Col key={exercises.exercise_id} xs={12} md={4} lg={3}>
+                        <PullPageCard exercise={exercise} />
+                    </Col>
+                ))
+            ) : (
+                <Col xs={12}>
+                    <p>No exercises found.</p>
+                </Col>
+            )}
+        </Row>
+    )}
+    {error && <div>Error: {error.message}</div>}
     </>
-  );
+)
 }
 
 export default PullPage;
--- src/WebPages/PullPage/PullPageCard.js
+++ src/WebPages/PullPage/PullPageCard.js
@@ -8,13 +8,13 @@ a text,
 and a button to go directly to that exercise's web page 
 */
 
-function PullPageCard() {
+function PullPageCard({ exercise }) {
   return (
     <>
       <Card className="mx-auto my-3" style={{ width: "18rem", backgroundColor: "black", color: "white" }}>
         <img className="card-img-top" src="..." alt="Pull exercise" />
         <div className="card-body">
-          <h5 className="card-title">Pull Page Card</h5>
+          <h5 className="card-title">{exercise.exercise_name}</h5>
           <p className="card-text">
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
