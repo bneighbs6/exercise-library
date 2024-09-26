739c5b7036bc97b09625afa3f44946e5c9f11cea
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-09-26 11:59:19 -0600

    Updated KneePageCard with ExercisePage

--- src/WebPages/HipPage/HipExercisePage.js
+++ src/WebPages/HipPage/HipExercisePage.js
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Row, Col } from "react-bootstrap";
-import { useParams } from "react-router-dom";
 import HipPageCard from "./HipPageCard";
 import ExercisePage from "../../Components/ExercisePage";
 
